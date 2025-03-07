94971bda83a24feda546bff6fdbfa0cfe183bf2e
Unish Rajkarnikar <ubidy947@gmail.com>
2025-03-06 19:43:45 -0600

    chore: updated ErrorMessage, added all exceptions to the advisor

--- src/main/java/org/thoughtlabs/blogbackend/advice/ErrorMessage.java
+++ src/main/java/org/thoughtlabs/blogbackend/advice/ErrorMessage.java
@@ -2,14 +2,16 @@ package org.thoughtlabs.blogbackend.advice;
 
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
 public class ErrorMessage {
     private int statusCode;
-    private long timestamp;
+    private LocalDateTime timestamp;
     private String message;
     private String description;
 
-    public ErrorMessage(int statusCode, long timestamp, String message, String description) {
+    public ErrorMessage(int statusCode, LocalDateTime timestamp, String message, String description) {
         this.statusCode = statusCode;
         this.timestamp = timestamp;
         this.message = message;
